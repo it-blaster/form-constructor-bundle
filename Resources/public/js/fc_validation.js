2b98f139272f17e514223899f50fe306ed2c81c9
Гашук Ярослав <y.gashuk@artsofte.ru>
2015-09-03 15:30:16 +0500

    Front-end validation WIP

--- Resources/public/js/fc_validation.js
+++ Resources/public/js/fc_validation.js
@@ -5,47 +5,41 @@ var FCValidationClass = function(form) {
 };
 
 FCValidationClass.prototype.addConstraint = function(constraint) {
-    this.constraints[constraint.id] = constraint;
+    if (typeof this.constraints[constraint.field] === 'undefined') {
+        this.constraints[constraint.field] = [];
+    }
+
+    this.constraints[constraint.field].push(constraint);
 
     return this;
 };
 
 FCValidationClass.prototype.enableConstraint = function(field, type) {
-    return this._setConstraintsActive(true, { field: field, type: type });
+    return this._setConstraintsActive(true, field, type);
 };
 
 FCValidationClass.prototype.disableConstraint = function(field, type) {
-    return this._setConstraintsActive(false, { field: field, type: type });
+    return this._setConstraintsActive(false, field, type);
 };
 
 FCValidationClass.prototype.enableConstraints = function(field) {
-    return this._setConstraintsActive(true, { field: field });
+    return this._setConstraintsActive(true, field);
 };
 
 FCValidationClass.prototype.disableConstraints = function(field) {
-    return this._setConstraintsActive(false, { field: field });
+    return this._setConstraintsActive(false, field);
 };
 
-FCValidationClass.prototype._setConstraintsActive = function(active, filter) {
-    for (var i in this.constraints) {
-        if (this._constraintFiltered(this.constraints[i], filter)) {
-            this.constraints[i].active = active;
-        }
-    }
-
-    return this;
-};
-
-FCValidationClass.prototype._constraintFiltered = function(constraint, filter) {
-    if (typeof filter === 'object') {
-        for (var i in filter) {
-            if (constraint[i] !== filter[i]) {
-                return false;
+FCValidationClass.prototype._setConstraintsActive = function(active, field, type) {
+    if (typeof this.constraints[field] === 'object') {
+        for (var i in this.constraints[field]) {
+            if (typeof type === 'undefined' || this.constraints[field][i].type === type) {
+                this.constraints[field][i].active = active;
             }
         }
     }
 
-    return true;
+    return this;
 };
 
 FCValidationClass.prototype.formIsValid = function($scope) {
@@ -53,6 +47,27 @@ FCValidationClass.prototype.formIsValid = function($scope) {
         $scope = $('form[name="'+ this.form +'"]');
     }
 
-    // TODO
+    var fields = this._findFields($scope);
+
+    // TODO: Получаем значение поля и валидируем его
+    // TODO: Если все ок - убираем ошибки, если нет - вешаем
+
     return true;
+};
+
+FCValidationClass.prototype._findFields = function($scope) {
+    var fields = {};
+
+    for (var field_name in this.constraints) {
+        var $field = $scope.find('[name^="'+ this.form +'['+ field_name +']"]');
+
+        if ($field.length) {
+            fields[field_name] = {
+                elements:    $field,
+                constraints: this.constraints[field_name]
+            };
+        }
+    }
+
+    return fields;
 };
